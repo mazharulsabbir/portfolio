80bdde7adcd35d1de2c3eb17c059b754fbff38bf
mazharulsabbir <mazharulsabbir2@gmail.com>
2020-05-11 12:12:52 +0600

    about me

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -10,7 +10,12 @@ export class ProfileComponent implements OnInit {
   clickCounter: number = 0;
   name: string
   title = 'Mazharul Sabbir';
-  
+  about_me = "ABOUT ME"
+  who_am_i = "WHO AM I"
+  about_body = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam laudantium quisquam voluptates eveniet? Placeat vero veritatis deleniti nemo delectus doloribus illum cum beatae recusandae, nihil velit vitae adipisci optio aliquam?" +
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam laudantium quisquam voluptates eveniet? Placeat vero veritatis deleniti nemo delectus doloribus illum cum beatae recusandae, nihil velit vitae adipisci optio aliquam?" +
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam laudantium quisquam voluptates eveniet? Placeat vero veritatis deleniti nemo delectus doloribus illum cum beatae recusandae, nihil velit vitae adipisci optio aliquam?"
+
   projects = [
     {
       name: "Rannaghor",
